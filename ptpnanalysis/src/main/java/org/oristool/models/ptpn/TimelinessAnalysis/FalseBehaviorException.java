68c53b4825b9f774fcb0809209d8a1ed0ac0b755
Federico <federico.chiesa@me.com>
2023-01-30 11:15:21 +0100

    Delete ptpnanalysis/src directory

--- ptpnanalysis/src/main/java/org/oristool/models/ptpn/TimelinessAnalysis/FalseBehaviorException.java
+++
@@ -1,8 +0,0 @@
-package org.oristool.models.ptpn.TimelinessAnalysis;
-
-
-public class FalseBehaviorException extends Exception {
-public FalseBehaviorException(String str) {
-		super("False Behavior: " + str);
-	}
-}
--- ptpnanalysis/src/main/java/org/oristool/models/ptpn/TimelinessAnalysis/ShortestPathEvaluator.java
+++
@@ -1,6 +0,0 @@
-package org.oristool.models.ptpn.TimelinessAnalysis;
-
-public interface ShortestPathEvaluator{
-	
-	public double[] getOptimizedResult(GlobalSet matrix, double[] objectiveFunctionCoefficients) throws FalseBehaviorException;
-}
